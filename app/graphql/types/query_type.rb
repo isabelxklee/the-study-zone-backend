44e6ec6c0263c516bb63cc5718499f831c3d8b5b
isabelxklee <hi@kleetime.com>
2022-09-13 11:52:40 -0400

    updated query type and algorithms controller

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,6 +7,12 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    field :algorithm, AlgorithmType
+      description: "Find an algorithm"
+    def algorithm(id:)
+      Algorithm.find(:id)
+    end
+
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
