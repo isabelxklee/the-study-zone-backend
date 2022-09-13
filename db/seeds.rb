bcb8213b4a10f43177a78d9e5e1dce40b3028446
isabelxklee <hi@kleetime.com>
2022-09-12 21:12:55 -0400

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Category.destroy_all
+DifficultyLevel.destroy_all
+
+Category.create(title: "String")
+Category.create(title: "Array")
+Category.create(title: "Linked List")
+Category.create(title: "Binary Tree")
+
+DifficultyLevel.create(title: "Easy")
+DifficultyLevel.create(title: "Medium")
+DifficultyLevel.create(title: "Hard")
+
+puts "data has been seeded! 🍃"
