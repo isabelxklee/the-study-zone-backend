d0a6d5a14ab70e4096b0e6ee7824a045b4cb4eb6
isabelxklee <hi@kleetime.com>
2022-09-20 11:35:02 -0400

    created seed data

--- rails-app/db/seeds.rb
+++ rails-app/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Category.destroy_all
+Difficulty.destroy_all
+# Algorithm.destroy_all
+
+cat_string = Category.create(name: "String")
+cat_array = Category.create(name: "Array")
+cat_linked_list = Category.create(name: "Linked List")
+cat_binary_tree = Category.create(name: "Binary Tree")
+
+lvl1 = Difficulty.create(name: "Easy")
+lvl2 = Difficulty.create(name: "Medium")
+lvl3 = Difficulty.create(name: "Hard")
+
+# algo1 = Algorithm.create(title: "Test", description: "This is a test.", category_id: cat_string.id, difficulty_level_id: lvl1.id)
+
+# Solution.create(explanation: "This is a test.", time_complexity: "O(n)", space_complexity: "O(n)", algorithm_id: algo1.id)
+
+puts "data has been seeded! 🍃"
