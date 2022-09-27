7cc79f33d57737232cc94abb975ce8efdcc93048
isabelxklee <hi@kleetime.com>
2022-09-26 22:14:39 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+
   post "/graphql", to: "graphql#execute"
+
+  get "/algorithms", to: "algorithms#index"
+  get "/categories", to: "categories#index"
+  get "/difficulties", to: "difficulties#index"
+
   resources :algorithms
   resources :difficulties
   resources :categories
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
