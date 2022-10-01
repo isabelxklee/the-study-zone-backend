1570da8727756eb4fea02454ea4824068af54ffb
isabelxklee <hi@kleetime.com>
2022-10-01 16:02:50 -0400

    added solution field to algorithm type

--- app/graphql/types/algorithm_type.rb
+++ app/graphql/types/algorithm_type.rb
@@ -5,6 +5,7 @@ module Types
     field :id, ID, null: false
     field :name, String
     field :description, String
+    field :solution, String
     field :category, Types::CategoryType
     field :difficulty, Types::DifficultyType
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,14 @@ easy = Difficulty.create(name: "Easy")
 medium = Difficulty.create(name: "Medium")
 hard = Difficulty.create(name: "Hard")
 
-Algorithm.create(name: "Two Sum", description: "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.", category_id: array.id, difficulty_id: easy.id)
+Algorithm.create(name: "Two Sum", description: "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.", category_id: array.id, difficulty_id: easy.id, solution: "# Hello, *world*!")
 
-Algorithm.create(name: "Palindrome Pairs", description: "Given a list of unique words, return all the pairs of the distinct indices (i, j) in the given list, so that the concatenation of the two words words[i] + words[j] is a palindrome.", category_id: string.id, difficulty_id: hard.id)
+Algorithm.create(name: "Palindrome Pairs", description: "Given a list of unique words, return all the pairs of the distinct indices (i, j) in the given list, so that the concatenation of the two words words[i] + words[j] is a palindrome.", category_id: string.id, difficulty_id: hard.id,
+  solution: "> **Cornix** stagna imagine mirarique in favilla recordor flammiferas lucem
+  > tamen legesque manant. Suadeat inpediunt latoque habent et noctis neque, mox
+  > aliqua at restitit! Sustinet sacrilegos perdidimus tulit veluti rogabat saepe
+  > non. Exanimis iustamque solutis Polypemonis, nitore deficit urbe, ter ait et."
+)
 
 Algorithm.create(name: "Trapping Rain Water", description: "Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.", category_id: array.id, difficulty_id: hard.id)
 
