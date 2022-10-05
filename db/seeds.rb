fd23b80b7bc0ccf8ebb98f2e5bdd1684a466db25
isabelxklee <hi@kleetime.com>
2022-10-05 12:02:21 -0400

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,29 +36,31 @@ Algorithm.create(name: "Trapping Rain Water", description: "Given n non-negative
 
 The amount of rain water at any given index is: the lower of the highest bars around it, minus the hight of the index.
 
-```
-rainWater = Math.min(highestLeftOfIndex, highestRightOfIndex) - indexHeight
+```javascript
+rainWater = Math.min(highestLeftOfIndex, highestRightOfIndex) - indexHeight;
 ```
 
 _The time complexity of this solution is O(n) linear time._
 
 ---
 
-### Overview
+### Overview of Steps
 
 - Set 2 pointers that start at each end of the array.
-- As the move towards the middle, they will find the highest right and left values, thus allowing us to calculate the level of rain water at each index.
+- As they move towards the middle, they will find the highest right and left values, thus allowing us to calculate the level of rain water at each index.
 - Then we can add each rain water value to a running total and return that value at the end.
+
+---
 
 ### Steps
 
 1. Create variables for storing the following values:
 
-- the total amount of rain water
-- left and right pointers
-- max values on the left and right side
+- The total amount of rain water
+- Left and right pointers
+- Max values on the left and right side
 
-_Note: Since the right pointer is going to start at the end of the array, we're going to set it to the last index in the array._
+> _Note: Since the right pointer is going to start at the end of the array, we're going to set it to the last index in the array._
 
 ```javascript
 const trappingRainWater = (arr) => {
@@ -67,7 +69,7 @@ const trappingRainWater = (arr) => {
   let rightPointer = arr.length - 1;
   let maxLeft = 0;
   let maxRight = 0;
-}
+};
 ```
 
 2. Create a `while` loop that runs while the left pointer is smaller or equal to the right pointer. We want this loop to run until the left and right pointers meet in the middle of the array.
@@ -86,7 +88,147 @@ const trappingRainWater = (arr) => {
 }
 ```
 
-### Full solution
+3. Inside the `while` loop, we're going to write some logic that updates the max values on the left and right side.
+
+First, we're going to check if the max left value is _less than_ the element at the left pointer's index.
+
+> _Note: I'm using a ternary operator instead of an `if/else` statement._
+
+```javascript
+const trappingRainWater = (arr) => {
+  ...
+
+  while (leftPointer <= rightPointer>) {
+    maxLeft < arr[leftPointer] ? // something else goes here
+  }
+}
+```
+
+If it _is_ less than, we're going to reassign the max left value to be the element's value at the left pointer.
+
+```javascript
+const trappingRainWater = (arr) => {
+  ...
+
+  while (leftPointer <= rightPointer>) {
+    maxLeft = maxLeft < arr[leftPointer] ? arr[leftPointer] : // something else will go here
+  }
+}
+```
+
+Otherwise, we're going to just keep the same max left value.
+
+```javascript
+const trappingRainWater = (arr) => {
+  ...
+
+  while (leftPointer <= rightPointer>) {
+    maxLeft = maxLeft < arr[leftPointer] ? arr[leftPointer] : maxLeft
+  }
+}
+```
+
+Now, let's repeat the same process for the max value on the right.
+
+```javascript
+const trappingRainWater = (arr) => {
+  ...
+
+  while (leftPointer <= rightPointer>) {
+    maxLeft = maxLeft < arr[leftPointer] ? arr[leftPointer] : maxLeft
+    maxRight = maxRight < arr[rightPointer] ? arr[rightPointer] : maxRight;
+  }
+}
+```
+
+4. Now that we've written some logic for updating our left and right max values, let's figure out how to update our running tally of total rain water.
+
+At this point, we're going to be implementing the solution that we saw at the top:
+
+```javascript
+rainWater = Math.min(highestLeftOfIndex, highestRightOfIndex) - indexHeight;
+```
+
+Firstly, define an `if/else` statement. We're going to check if the max left value is greater than the max right value.
+
+```javascript
+const trappingRainWater = (arr) => {
+  ...
+
+  while (leftPointer <= rightPointer>) {
+    maxLeft = maxLeft < arr[leftPointer] ? arr[leftPointer] : maxLeft
+    maxRight = maxRight < arr[rightPointer] ? arr[rightPointer] : maxRight;
+
+    if (maxLeft > maxRight) {
+      // something goes here
+    } else {
+      // something goes here
+    }
+  }
+}
+```
+
+If it _is_ greater than, we're going to subtract the element at the right pointer's index from the max right value. Then we're going to add this to the total rain water.
+
+```javascript
+const trappingRainWater = (arr) => {
+  ...
+
+  while (leftPointer <= rightPointer>) {
+    maxLeft = maxLeft < arr[leftPointer] ? arr[leftPointer] : maxLeft
+    maxRight = maxRight < arr[rightPointer] ? arr[rightPointer] : maxRight;
+
+    if (maxLeft > maxRight) {
+      totalWater += maxRight - arr[rightPointer];
+    } else {
+      // something goes here
+    }
+  }
+}
+```
+
+And then we're going to move the right pointer one step towards the middle by decrementing its value.
+
+```javascript
+const trappingRainWater = (arr) => {
+  ...
+
+  while (leftPointer <= rightPointer>) {
+    maxLeft = maxLeft < arr[leftPointer] ? arr[leftPointer] : maxLeft
+    maxRight = maxRight < arr[rightPointer] ? arr[rightPointer] : maxRight;
+
+    if (maxLeft > maxRight) {
+      totalWater += maxRight - arr[rightPointer];
+      rightPointer --;
+    } else {
+      // something goes here
+    }
+  }
+}
+```
+
+5. And then we're going to repeat some similar logic for the `else` clause.
+
+```javascript
+const trappingRainWater = (arr) => {
+  ...
+
+  while (leftPointer <= rightPointer>) {
+    maxLeft = maxLeft < arr[leftPointer] ? arr[leftPointer] : maxLeft
+    maxRight = maxRight < arr[rightPointer] ? arr[rightPointer] : maxRight;
+
+    if (maxLeft > maxRight) {
+      totalWater += maxRight - arr[rightPointer];
+      rightPointer --;
+    } else {
+      totalWater += maxLeft - arr[leftPointer];
+      leftPointer++;
+    }
+  }
+}
+```
+
+6. Lastly, we're just going to return our total rain water.
 
 ```javascript
 const trappingRainWater = (arr) => {
@@ -111,7 +253,7 @@ const trappingRainWater = (arr) => {
 
   return totalWater;
 };
-``` 
+```   
 ")
 
 Algorithm.create(name: "Binary Tree Paths", description: "Given the root of a binary tree, return all root-to-leaf paths in any order. A leaf is a node with no children.", category_id: binary_tree.id, difficulty_id: easy.id)
